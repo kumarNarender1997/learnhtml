e3ebafc04407a9aefc1dbe5a20e0d5e9ee0cc08c
kumarNarender1997 <nvnsingh413@gmail.com>
2022-04-05 13:11:53 +0530

    git

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("hi i am narender kumar");
+var name ='Narender kumar';
+console.log(name);
+name ='Narender kumar parjapati';
+let sirname ='parjapati'; //We can change the value using let variable
+console.log(sirname);
+sirname ='kumar';
+console.log(sirname);
+const age=28;
+console.log(age);
+age =30; // We can not change the value using const variable
+console.log(age);
+
